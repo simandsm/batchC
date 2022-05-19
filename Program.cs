77f38e360243ea8f7fb83003faf3f58465881d4f
simandsm <77308422+simandsm@users.noreply.github.com>
2022-05-19 16:44:06 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace 批量修改
+{
+	internal static class Program
+	{
+		[STAThread]
+		private static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MYForm());
+		}
+	}
+}
